b71f4f9cd7b40f95c022b824ec548d2965f24caf
qjerry <qjerryxjh@163.com>
2019-08-25 13:21:08 +0800

    add the resetPwd api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const controller = require('../controller')
 
 const router = new Router()
 
-router.post('/api/register', controller.user.register)
+router
+  .post('/api/register', controller.user.register)
   .post('/api/login', controller.user.login)
+  .post('/api/resetPwd', controller.user.resetPwd)
 
 module.exports = router
